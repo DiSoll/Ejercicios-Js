e6e6761879b44dd4a0ed66559b74b8bd083ca2fa
Diana Salazar <dayanasoll@hotmail.com>
2023-06-22 21:30:06 -0300

    desarrollo del ejercicio1

--- ejercicios.js
+++ ejercicios.js
@@ -1,14 +1,20 @@
 // EJERCICIO 1
+let lista1 = [2, 4, 6, 8];
+let lista2 = [3, 5, 7, 9];
+let lista3 = [1, 2, 3, 4];
+let listaDeListas = [lista1,lista2,lista3]
 
-function summaArray(numeros) {
+
+function summaArray(cualquierLista) {
     let suma = 0;
-    for (let i = 0; i < numeros.length; i++) {
-        suma = suma + numeros[i];
+    for (let i = 0; i < cualquierLista.length; i++) {
+        suma = suma + cualquierLista[i];
     }
+
     return suma;
 }
 
-const numeros = [1, 2, 3, 4, 5];
+
 
 
 // *OTRA FORMA DE VER RESULTADO 
@@ -17,4 +23,15 @@ const numeros = [1, 2, 3, 4, 5];
 // console.log(resultadodeSuma);
 
 
-console.log(summaArray(numeros));
+// console.log(summaArray(lista1));
+// console.log(summaArray(lista2));
+// console.log(summaArray(lista3));
+
+
+
+for (let i = 0; i < listaDeListas.length; i++) {
+   console.log(summaArray(listaDeListas[2]));
+    
+}
+
+
